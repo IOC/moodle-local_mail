c1d4be816c3d9c3ac42a1cc4a4b6d19b13a938d1
Albert Gasset <albert.gasset@gmail.com>
2013-02-07 16:17:46 +0100

    Preparations for release

--- version.php
+++ version.php
@@ -1,4 +1,8 @@
 <?php
 
-$plugin->version = 2012092600;
+$plugin->version = 2013020700;
+$plugin->requires = 2012062500;
 $plugin->cron = 0;
+$plugin->component = 'local_mail';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0';
