798bcb14753eccfff7cc908e41a852afc092010f
Albert Gasset <albert.gasset@gmail.com>
2013-06-17 17:43:28 +0200

    Release v1.3

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 
-$plugin->version = 2013041600;
+$plugin->version = 2013061700;
 $plugin->requires = 2012062500;
 $plugin->cron = 0;
 $plugin->component = 'local_mail';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.2';
+$plugin->release = '1.3';
