ccd9efc9d4611c25d5af59ab1662d8f9c6e38294
Marc Català <reskit@gmail.com>
2014-03-28 09:27:09 +0100

    Release v1.4

--- version.php
+++ version.php
@@ -26,4 +26,4 @@ $plugin->requires = 2012062500;
 $plugin->cron = 0;
 $plugin->component = 'local_mail';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.3.1';
+$plugin->release = '1.4';
