be747ce2b05bf31b330eb6c92395065074a99c33
Marc Català <mcatala@itteria.cat>
2016-01-12 09:25:11 +0100

    Release v1.8.1

--- version.php
+++ version.php
@@ -21,9 +21,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version = 2015121600;
+$plugin->version = 2016011200;
 $plugin->requires = 2015051100;
 $plugin->cron = 0;
 $plugin->component = 'local_mail';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.8';
+$plugin->release = '1.8.1';
